0c96ac01bbf9015b63170ade1f8d1fbc3e3fb947
mhlee80 <myeonghyeon.lee@gmail.com>
2019-03-26 13:31:36 +0900

    index.html의 버튼에 액션 추가(index.js)

---
+++ src/index.js
@@ -0,0 +1,13 @@
+function launchSignIn(eventObj) {
+    window.location.href = "sign_in.html";
+}
+
+function launchChangePassword(eventObj) {
+    window.location.href = "change_password.html";
+}
+
+let signInButton = document.getElementById('index_sign_in_button');
+signInButton.addEventListener('click', launchSignIn);
+
+let changePasswordButton = document.getElementById('index_change_password_button');
+changePasswordButton.addEventListener('click', launchChangePassword);
