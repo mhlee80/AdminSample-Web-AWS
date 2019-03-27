78fdf1006d5425630f587a8afa55fd5dbcea7b58
mhlee80 <myeonghyeon.lee@gmail.com>
2019-03-27 11:12:43 +0900

    change password 화면 및 로직 구현

---
+++ src/change_password.js
@@ -0,0 +1,20 @@
+(function scopeWrapper(auth) {
+    let changePasswordForm = document.getElementById('change_password_form');
+    changePasswordForm.addEventListener('submit', changePassword);
+
+    function changePassword(event) {
+        event.preventDefault();
+
+        let email = document.getElementById('change_password_email_input').value;
+        let originPassword = document.getElementById('change_password_origin_password_input').value;
+        let newPassword = document.getElementById('change_password_new_password_input').value;
+
+        auth.changePassword(email, originPassword, newPassword,
+            function onSuccess() {
+                alert('success');
+            },
+            function onFailure(err) {
+                alert(`failure: ${err}`);
+            });
+    }
+}(cognitoAuth));
