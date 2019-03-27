af9f1ced437882783017449281131b9be1330ad3
mhlee80 <myeonghyeon.lee@gmail.com>
2019-03-27 16:51:45 +0900

    AWS cognito 설정 변경에 따른 코드 업데이트
    AWS cognito에서 AdminSample 사용자 풀 생성
    AWS cognito의 AdminSample 사용자 풀에서 AdminSampleAppClient 앱 클라이언트 추가
    해당사항을 config 파일에 적용

--- src/aws_config.js
+++ src/aws_config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-northeast-2_eOfhIIUXT', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4ae58e8oadlf7lcsl090lq30sv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-northeast-2_heM71Rp6Y', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4bvdj7jcs7gngq7ee35nm11m5g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
