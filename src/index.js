6156c60d8cecef4016ac1977c373b88123ba35e7
mhlee80 <myeonghyeon.lee@gmail.com>
2019-03-27 11:00:59 +0900

    Index.html, index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-function launchSignIn(eventObj) {
-    window.location.href = "sign_in.html";
-}
+(function scopeWrapper() {
+    function launchSignIn(eventObj) {
+        window.location.href = "sign_in.html";
+    }
 
-function launchChangePassword(eventObj) {
-    window.location.href = "change_password.html";
-}
+    function launchChangePassword(eventObj) {
+        window.location.href = "change_password.html";
+    }
 
-let signInButton = document.getElementById('index_sign_in_button');
-signInButton.addEventListener('click', launchSignIn);
+    let signInButton = document.getElementById('index_sign_in_button');
+    signInButton.addEventListener('click', launchSignIn);
 
-let changePasswordButton = document.getElementById('index_change_password_button');
-changePasswordButton.addEventListener('click', launchChangePassword);
+    let changePasswordButton = document.getElementById('index_change_password_button');
+    changePasswordButton.addEventListener('click', launchChangePassword);
+}());
